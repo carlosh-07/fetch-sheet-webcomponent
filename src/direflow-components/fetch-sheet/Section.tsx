232f1a45009fce4ebafa416d51506416ca6e38f8
carlosh-07 <95hernandezcarlos@gmail.com>
2022-03-05 13:43:32 -0500

    improve

--- src/direflow-components/fetch-sheet/Section.tsx
+++ src/direflow-components/fetch-sheet/Section.tsx
@@ -5,16 +5,16 @@ interface StyledSection {
 }
 
 const Section = styled.section<StyledSection>`
-  color: ${(props) => (props.index === 3 ? "#EEAA00" : "white")};
+  color: ${(props) => (props.index === 2 ? "WHITE" : "#EEAA00")};
   background-color: ${(props) => {
     if (props.index === 1) {
-      return "white";
-    }
-    if (props.index === 2) {
       return "#653819";
     }
+    if (props.index === 2) {
+      return "#EEAA00";
+    }
 
-    return "#EEAA00";
+    return "WHITE";
   }};
   padding-bottom: 10px;
 `;
